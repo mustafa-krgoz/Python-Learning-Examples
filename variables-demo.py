6bae4b12f919656fe1e3588c62a4b2a0bdbef949
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-07-15 17:46:55 +0300

    Add files via upload

---
+++ variables-demo.py
@@ -0,0 +1,33 @@
+"""
+  1. Müşterinin aşağıdaki bilgileri için değişken oluşturunuz.
+    Müşterinin adı
+    Müşterinin soyadı
+    Müşteri ad + soyad
+    Müşteri cinsiyet
+    Müşteri tc kimlik
+    Müşteri doğum yılı
+    Müşteri adres bilgisi
+    Müşteri yaşı
+ """
+"""
+    2. Aşağıdaki müşterilerin toplam bilgisini hesaplayınız.
+        Sipariş 1 ===> 110 Tl
+        Sipariş 2 ===> 1100.5 Tl
+        Sipariş 3 ===> 356.95 Tl
+"""
+musteriAdi = 'Mustafa'
+musteriSoyadi = 'Karagöz'
+musteriCinsiyet = True #Erkek
+musteriTcKimlik = '50682480815'
+musteriDogumYili = 2002
+musteriAdresBiligisi = "Diyarbakır Yenişehir"
+musteriYasi = '20'
+
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+
+total = order1 + order2 + order3
+print("Total:", total)
+
+
