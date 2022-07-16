175351554e62e3038e84a7eee9a937cd991d0ce7
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-07-16 14:34:16 +0300

    Add files via upload

---
+++ type-conversion2.py
@@ -0,0 +1,36 @@
+from unittest import result
+
+
+x = 2   # int
+y = 5.2 # double
+name = 'Mustafa'  #str
+isOnline = True #bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+    #Type Conversion
+# int to float
+    # x = float(x)
+    # print(x)
+    # print(type(x))
+# float to int
+    # y = int(y)
+    # print(y)
+    # print(type(y))
+# String
+    # result = str(x) + str(y)
+    # print(type(result))
+    # print(result)
+# bool to str
+    # isOnline = str(isOnline)
+    # print(isOnline)
+    # print(type(isOnline))
+# bool to int
+    # isOnline = False
+    # isOnline = int(isOnline)
+    # print(isOnline)
+    # print(type(isOnline))
+
