2dcd32412b0ad5f176cf47f309ead7d448ed9ec6
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-07-17 16:26:20 +0300

    Add files via upload

---
+++ type-conversion-demo.py
@@ -0,0 +1,15 @@
+'''
+    Dairenin alanı = pi*r*r
+    Dairenin çevresi = 2*pi*r
+
+    Yarıçapı girilen bir dairenin alanını ve çevresini hesaplayınız.
+
+'''
+r = float(input("Yarıçap:"))
+pi = 3.14
+
+daireninCevresi = 2*pi*r
+daireninAlani = pi*r*r
+
+print("Dairenin Alanı:" , daireninAlani, "Dairenin Çevresi:", daireninCevresi)
+
