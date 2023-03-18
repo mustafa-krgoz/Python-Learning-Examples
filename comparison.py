fbafd0f087c44d97310d90f7267389ec62bf8cae
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-03-18 16:27:31 +0300

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,19 @@
+# username, password => database
+# 'mustafakaragoz' , '123321'
+
+a, b, c, d = 5, 5, 10, 4
+
+username = 'mustafa'
+password = 12345
+
+result = (a==b) # a, b'ye eşit mi?  True
+result = (a==c) # False
+result = ('delaney' == username) # False
+result = (a>b) # False
+result = (a>=b) # True
+result = (True == 1) # Nümerik olarak true 1'e eşittir.
+result = (False == 0) # Nümerik olarak false 0' a eşittir.
+result = (True + False + 40)
+result = (a!=b) # a, b' ye eşit değil mi?
+
+print(result)
