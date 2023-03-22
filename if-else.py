3154225e6a71ca2af1a2835c294fa8e2cc576677
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-03-22 10:19:14 +0300

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,19 @@
+# username = "mustafa"
+# password = 4567
+
+# isLoggedin = (username == 'mustafa') and (password == 4567)
+
+# if isLoggedin:
+#     print("Hoş geldiniz") # İf in içerisi bit tab boşluklu olacak:
+
+
+username = 'krgoz'
+password = 123
+
+if (username == 'krgoz'):
+    if(password == 1234):
+        print("Başarılı bir şekilde girdiniz.")
+    else:
+        print('Parola yanlış')
+else:
+    print('Kullanici adi yanlış.')
