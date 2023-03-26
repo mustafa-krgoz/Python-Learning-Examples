efb352884b1123e200ecf43d0436740cd5102dc4
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-03-26 11:50:12 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,22 @@
+
+message = 'My name is Mustafa Karagöz'.split()
+print(message[0])
+
+# my_list = [1,2,3]
+my_list = ['bir', 2, True, 5.6]
+print(my_list)
+
+
+list1 = ['one', 'two', 'three']
+list2 = ['four', 'five', 'six']
+ 
+numbers = list1 + list2
+print(numbers)
+print(len(numbers))
+
+userA = ['Mustafa', 20]
+userB = ['Diren', 32]
+
+users = [userA, userB]
+print(users)
+print(users[0][1])
