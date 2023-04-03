5dea37ede2b3101516af4ee30f4c7989fe6ef21e
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-04-03 18:39:19 +0300

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+count = 0
+total = 0
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done':
+        break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+average = total / count
+print('Average: ', average)
