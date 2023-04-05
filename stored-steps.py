b3d8ea6fca96f3f068e78d4ab892d656ce42773a
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-04-05 15:07:25 +0300

    Add files via upload

---
+++ stored-steps.py
@@ -0,0 +1,9 @@
+#Stored(and reused) Steps = Saklanan(ve yeniden kullanılan) Adımlar
+
+def thing():
+    print('Hello')
+    print('Fun')
+
+thing() # ilk Hello fun
+print('Zip')
+thing() # ikinci Hello fun
