7b9b5ec6c9e4d2b8ad724d0872778d2bb9b36a21
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-04-08 14:15:29 +0300

    Add files via upload

---
+++ strings-methods.py
@@ -0,0 +1,19 @@
+message = "Hello there. My name is Mustafa Karagöz"
+#message = message.upper()
+#message = message.lower()
+#message = message.capitalize()
+#message = message.title()
+#message = message.strip()
+#message = message.split()
+#message = message.split('.')
+#message = '*'.join(message)
+#index = message.find("Mustafa")
+#isFound = message.startswith('H')
+#isFound = message.endswith('z')
+message = message.center(50,'*')
+
+
+#print(isFound)
+#print(index)
+print(message)
+#print(message[2])
